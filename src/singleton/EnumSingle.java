b59510ab63bca51f76110ec045261cc6decb12bc
Varun Dixit <varun.me15@gmail.com>
2015-03-06 13:48:48 -0800

    EnumSingleton

---
+++ src/singleton/EnumSingle.java
@@ -0,0 +1,10 @@
+package singleton;
+
+public enum EnumSingle {
+
+	ENUMINSTANCE;
+	
+	public void testFunction(){
+		System.out.println("Checking Singleton");
+	}
+}
---
+++ src/singleton/SingleTest.java
@@ -0,0 +1,14 @@
+package singleton;
+
+public class SingleTest {
+
+	
+
+	public static void main(String[] args) {
+		
+		
+		EnumSingle.ENUMINSTANCE.testFunction();
+
+	}
+
+}
